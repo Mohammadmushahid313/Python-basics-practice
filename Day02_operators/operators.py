66fa1a40514fe7a3181e2c5811afc4acffa9f757
Mohammad Mushahid <mohammadmushahid313@gmail.com>
2026-02-08 15:43:25 +0530

    Day02:Comparison and logical operators Completed

--- Day02_operators/operators.py
+++ Day02_operators/operators.py
@@ -7,3 +7,18 @@ print("division:",a/b)
 print("modulus:",a%b)
 print("Power:",a**b)
 print("floor diviusion:",a//b)
+print("\n......comparison operators....")
+a=10
+b=5
+print(a>b) #grater than
+print(a<b) #less than
+print(a==b) #equal to
+print(a!=b) # not equal 
+print(a>=b) # greater or eqaul
+print(a<=b) #less than equal
+print("\n.......logical operators.....")
+age=19
+has_id=True
+print(age>=18 and has_id)
+print(age<18 or has_id)
+print(not has_id)
