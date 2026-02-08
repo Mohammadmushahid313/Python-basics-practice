b58f67b5d6ce81226790b404b01fad447514a5ee
Mohammad Mushahid <mohammadmushahid313@gmail.com>
2026-02-08 15:19:32 +0530

    Day02:Arithmetic operators completed

---
+++ Day02_operators/operators.py
@@ -0,0 +1,9 @@
+a=20
+b=6
+print("addition:",a+b)
+print("substraction:",a-b)
+print("multiplication:",a*b)
+print("division:",a/b)
+print("modulus:",a%b)
+print("Power:",a**b)
+print("floor diviusion:",a//b)
