585c710b22a79dbbb38401c447fd2e507e79032a
Mohammad Mushahid <mohammadmushahid313@gmail.com>
2026-02-11 22:36:27 +0530

    Day03_Largest of 3 Numbers

---
+++ Day03_Conditional statements/Largest of 3 Number.py
@@ -0,0 +1,9 @@
+a=int(input("enter a first number"))
+b=int(input("enter a second number"))
+c=int(input("enter a third number"))
+if 10>=20 and 10>=30:
+ print("10 is the largest")
+elif 20>=10 and 20>=30:
+ print("20 is the largest")
+else:
+ print("30 is the largest")
