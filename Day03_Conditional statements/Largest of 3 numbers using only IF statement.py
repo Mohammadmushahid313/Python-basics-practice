85c630fafc9831cf2f97d59c4839a37b5a3caa6b
Mohammad Mushahid <mohammadmushahid313@gmail.com>
2026-02-11 22:50:21 +0530

    Day03_Largest of 3 numbers using only IF statement

---
+++ Day03_Conditional statements/Largest of 3 numbers using only IF statement.py
@@ -0,0 +1,10 @@
+a=int(input("enter a number:"))
+b=int(input("enter a number:"))
+c=int(input("enter a number:"))
+if 10<20 and 10<30:
+    print(a, "is largest number")
+if 20>30 and 20>10:      
+    print(b,"is largest number")
+if 30>10 and 30>10:
+    print(c,"is the largest number")
+
